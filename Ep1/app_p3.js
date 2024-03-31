ab1569bc1f19d9f7abdf043c49fe4c0d17d3e29d
Sunnymangat <jaskaran887200@gmail.com>
2024-03-31 10:21:10 +0530

    Foundation of ReactJs & How internals of js works.

---
+++ Ep1/app_p3.js
@@ -0,0 +1,5 @@
+const heading=React.createElement("h1",{id:"heading",xyb:"juhgjugh"},"Hello World.. From ReactJs");
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+console.log(root);
+root.render(heading);
